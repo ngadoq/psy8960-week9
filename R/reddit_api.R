47fc88582bef3b156934deb62c3f6d7bf7b3690e
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-03-25 18:05:57 -0500

    final commit

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -10,7 +10,7 @@ rstat_list <- fromJSON("https://www.reddit.com/r/rstats/.json", flatten = TRUE)
 # extract post information (children), convert to tibble
 rstats_original_tbl <- as_tibble(rstat_list$data$children)
 
-# Create tbl to only include post title, # up votes and # comments
+# Create tbl to only include post title, # upvotes and # comments
 rstats_tbl <- rstats_original_tbl %>% 
   select(post = data.title, upvotes = data.ups, comments = data.num_comments)
 
