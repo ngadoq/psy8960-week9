55d23f884b9284094f150ec7bff3f36200ca8852
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-03-25 16:47:47 -0500

    Visualization and analysis

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -14,4 +14,15 @@ rstats_original_tbl <- as_tibble(rstat_list$data$children)
 rstats_tbl <- rstats_original_tbl %>% 
   select(post = data.title, upvotes = data.ups, comments = data.num_comments)
 
+# Visualization
+# Use ggplot to create a scatterplot to show relationship between upvotes and comments
+(ggplot(rstats_tbl, aes(upvotes, comments))+
+    geom_point() +
+    xlab("Number of upvotes") +
+    ylab("Number of comments") ) |> 
+  ggsave(filename="../figs/fig1.png", units = "px", width = 1920, height = 1080)
+
+# Analysis
+# Use cor.test to calculate correlation between upvotes and comments and p-value
+cor.test(rstats_tbl$upvotes, rstats_tbl$comments)
 
