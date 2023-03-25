e0f150017f2c577393901c3b43f15c08fef66ce9
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-03-25 16:48:19 -0500

    Publication

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -26,3 +26,35 @@ rstats_tbl <- rstats_original_tbl %>%
 # Use cor.test to calculate correlation between upvotes and comments and p-value
 cor.test(rstats_tbl$upvotes, rstats_tbl$comments)
 
+
+# Publication 
+# The correlation between upvotes and comments was r(23) = .42, p = .038. This test was statistically significant.
+# function to display the output of the correlation
+correlation_fct <- function(upvotes, comments) {
+  # Calculate the correlation between upvotes and comments
+  cor_result <- cor.test(upvotes, comments)
+  corr <- cor_result$estimate
+  df <- cor_result$parameter
+  p_value <- cor_result$p.value
+  
+  # Format correlation and p-value to display only 2 digits after the decimal and remove leading zeros 
+  corr_f <- corr %>% 
+    formatC(digits=2) %>% 
+    str_remove("^0+")
+  p_value_f <- p_value %>% 
+    formatC(digits=2) %>% 
+    str_remove("^0+")
+  # Construct the outputted line of text using the template
+  if (p_value < 0.05) {
+    output <- paste("The correlation between upvotes and comments was r(", df, ") = ", corr_f, ", p = ", p_value_f, ". This test was statistically significant.", sep="")
+  } else {
+    output <- paste("The correlation between upvotes and comments was r(", df, ") = ", corr_f, ", p = ", p_value_f, ". This test was not statistically significant.", sep="")
+  }
+  
+  # Return the output
+  return(output)
+}
+
+# Run the function with data
+correlation_fct(rstats_tbl$upvotes, rstats_tbl$comments)
+
