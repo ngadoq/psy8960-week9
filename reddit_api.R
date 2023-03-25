7f145c45604f6d13313a30232d509a55b69fdf0e
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-03-25 15:37:02 -0500

    Initial commit

---
+++ reddit_api.R
@@ -0,0 +1,18 @@
+# Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(jsonlite)
+library(tidyverse)
+
+# use jsonlite to convert json to R list 
+rstat_list <- fromJSON("https://www.reddit.com/r/rstats/.json", flatten = TRUE)
+
+# extract post information (children), convert to tibble
+rstats_original_tbl <- as_tibble(rstat_list$data$children)
+
+# Create tbl to only include post title, # up votes and # comments
+rstats_tbl <- rstats_original_tbl %>% 
+  select(post = data.title, upvotes = data.ups, comments = data.num_comments)
+
+
+
+
